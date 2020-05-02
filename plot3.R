2c2307170a94d9f803a0ff6202afb284da4332dd
Rongeet <64375640+Rongeet@users.noreply.github.com>
2020-05-02 23:33:32 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -11,9 +11,17 @@ NEI <- readRDS("./air_pollution/summarySCC_PM25.rds")
 SCC <- readRDS("./air_pollution/Source_Classification_Code.rds")
 str(NEI)
 str(SCC)
+# Filter observations relating to Baltimore MD
+Baltimore <- subset(NEI, fips == "24510")
+
+# Total all emissions in Baltimore for the years 1999 to 2008
 typeBaltimore <- ddply(Baltimore, .(year, type), function(x) sum(x$Emissions))
- colnames(typeBaltimore)[3] <- "emissions"
- par(bg = 'grey')
- qplot(year, emissions, data = typeBaltimore, color = type, geom = "line") + ggtitle("PM2.5 Emission by Type and Year in Baltimore City") + xlab("Year") + ylab("Total PM2.5 Emissions in tons") + theme(legend.position = c(0.9, 0.85))
+
+# Rename the column which was modified by R
+colnames(typeBaltimore)[3] <- "emissions"
+
+# Plot to file
+par(bg = 'grey')
+qplot(year, emissions, data = typeBaltimore, color = type, geom = "line") + ggtitle("PM2.5 Emission by Type and Year in Baltimore City") + xlab("Year") + ylab("Total PM2.5 Emissions in tons") + theme(legend.position = c(0.9, 0.85))
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
